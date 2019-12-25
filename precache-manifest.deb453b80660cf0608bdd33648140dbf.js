cdedf00703bc6355068b29ee39a0d3e9cc70c83e
Rafael Makaha <rafael.makaha@gmail.com>
2019-12-25 11:20:15 -0300

    Updates

---
+++ precache-manifest.deb453b80660cf0608bdd33648140dbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4a9bb09b23fdda2291ba0a1ebba133",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45934eb157abe84273d1",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b4afabc62ef2ac09c7cd",
+    "url": "/static/js/2.7309100f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7309100f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45934eb157abe84273d1",
+    "url": "/static/js/main.e7fa517e.chunk.js"
+  },
+  {
+    "revision": "24e2c4bda444ec400fd9",
+    "url": "/static/js/runtime-main.1ca232a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
